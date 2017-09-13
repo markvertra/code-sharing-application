34e36b2dd8e4477327c720007ed68e955cfa45d0
ByronGBP <byrongbp@gmail.com>
2017-09-13 20:48:53 +0200

    setup profile and added some semicolon

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const User               = require('../../models/user')
-const Project            = require('../../models/project')
+const User               = require('../../models/user');
+const Project            = require('../../models/project');
 const bcrypt = require("bcrypt");
 
 router.get('/', (req, res, next) => {
     User.find({}, (err, users ) => {
-        if (err) { next(err)}
-    Project.find({}, (err, projects ) => {
-        if (err) { next(err)}
-    res.render('admin/admin', {users: users, projects: projects})
+        if (err) { next(err);}
+    Project.find({}, (err, projects) => {
+        if (err) { next(err);}
+    res.render('admin/admin', {users: users, projects: projects});
     });
     });
 });
+
+
 
 router.post('/createuser', (req, res, next) => {
     const newUser = new User({
@@ -20,43 +22,43 @@ router.post('/createuser', (req, res, next) => {
         password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)),
         email: req.body.email,
         role: "User"
-    })
-      
+    });
+
     newUser.save((err) => {
-        if (err) {return next(err)}
+        if (err) {return next(err);}
         res.redirect('/admin');
-    })
+    });
 });
 
 router.post('/edituser/:userID', (req, res, next) => {
-    
+
   const infoUser = {
     username: req.body.username,
     password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)),
     email: req.body.email,
   };
-  
+
   User.findByIdAndUpdate(req.params.userID, infoUser, (err, users) => {
 
-    if (err) { return next(err) }
+    if (err) { return next(err); }
     User.find({}, (err, users) => {
-      if (err) { return next(err) }
-      
+      if (err) { return next(err); }
+
       res.redirect('/admin');
   });
   });
-})
+});
 
 router.post('/deleteuser/:userID', (req, res, next) => {
     User.findByIdAndRemove(req.params.userID, (err, users) => {
-        if (err) { return next(err) }
+        if (err) { return next(err);}
         User.find({}, (err, users) => {
-        
-        if (err) { return next(err) }
-            res.redirect('/admin')
+
+        if (err) { return next(err);}
+            res.redirect('/admin');
       });
     });
-})
+});
 
 router.post('/createproject', (req, res, next) => {
     const newProject = new Project({
@@ -65,45 +67,45 @@ router.post('/createproject', (req, res, next) => {
         fileHTML: req.body.fileHTML,
         fileCSS: req.body.fileCSS,
         fileJS: req.body.fileJS,
-    })
-      
+    });
+
     newProject.save((err) => {
-        if (err) {return next(err)}
+        if (err) {return next(err);}
         res.redirect('/admin');
-    })
+    });
 });
 
 router.post('/editproject/:projectID', (req, res, next) => {
-    
+
     const infoProject = {
         projectName: req.body.projectName,
         userID: req.body.userID,
         fileHTML: req.body.fileHTML,
         fileCSS: req.body.fileCSS,
         fileJS: req.body.fileJS,
-    }
-  
+    };
+
   Project.findByIdAndUpdate(req.params.projectID, infoProject, (err, projects) => {
 
-    if (err) { return next(err) }
+    if (err) { return next(err);}
     Project.find({}, (err, projects) => {
-      if (err) { return next(err) }
-      
+      if (err) { return next(err);}
+
       res.redirect('/admin');
   });
   });
-})
+});
 
 router.post('/deleteproject/:projectID', (req, res, next) => {
     Project.findByIdAndRemove(req.params.projectID, (err, projects) => {
-        if (err) { return next(err) }
+        if (err) { return next(err);}
         Project.find({}, (err, projects) => {
-        
-        if (err) { return next(err) }
-            res.redirect('/admin')
+
+        if (err) { return next(err);}
+            res.redirect('/admin');
       });
     });
-})
+});
 
 
 module.exports = router;
