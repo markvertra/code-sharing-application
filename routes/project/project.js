430d593e53ed92fd42d9c0e9e2266dfd6420bf07
ByronGBP <byrongbp@gmail.com>
2017-09-14 16:36:48 +0200

    create new project sketch done

--- routes/project/project.js
+++ routes/project/project.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const Project = require('../../models/project');
+const User = require('../../models/user');
 
 router.get('/new', (req, res, next) => {
-  console.log(req);
   res.render('project/new');
 });
 
 router.post('/new', (req, res, next) => {
-    console.log(req.body);
+ //TODO:- Look if is login
+
+  const newProject = new Project({
+    projectName: req.body.projectName,
+    userID: req.user._id,
+    file: {fileHTML: req.body.fileHTML,
+           fileCSS: req.body.fileCSS,
+           fileJS: req.body.fileJS,
+         }
+    });
+
+  newProject.save((err) => {
+    if (err) {return next(err);}
+
+    User.findByIdAndUpdate(req.user._id,{
+        $push: {projectIDs: req.user._id} }, (err, user) => {
+        if (err) {return next(err);}
+        console.log(user);
+        res.redirect(req.headers.referer);
+      });
+  });
 });
 
 router.get('/:projectID', (req, res, next) => {
