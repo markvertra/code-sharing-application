8c021011bf0091784645181617fbfb6dc2763531
ByronGBP <byrongbp@gmail.com>
2017-09-14 16:03:49 +0200

    setup new project mvc

--- routes/project/project.js
+++ routes/project/project.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const Project = require('../../models/project');
 
+router.get('/new', (req, res, next) => {
+  console.log(req);
+  res.render('project/new');
+});
+
+router.post('/new', (req, res, next) => {
+    console.log(req.body);
+});
+
 router.get('/:projectID', (req, res, next) => {
   let projectID = req.params.projectID;
   Project.findById(projectID).populate('userID', 'username _id').exec((err, project) => {
