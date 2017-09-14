0eb47d52dbcedc9b3005a457c1f217dfd14c2c2a
Mark Allen <mark@traversify.com>
2017-09-14 16:48:09 +0200

    Login and signup go away with clicks elsewhere

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,38 @@
 $(document).ready(function(){
 
+    
+
+
     $(".login-btn").on("click", function(){
         $(".login-form").css("display", "block");
+        setTimeout(clickCloserTwo, 100);
     });
 
     $(".signup-link").on("click", function(){
-        $(".signup-box").css("display", "block");
-        $(".login-form").css("display", "none");
+        $("#signup-box").css("display", "block");
+        $(".login-form").css("display", "none"); 
+        setTimeout(clickCloser, 100);     
     });
 
     $(".close-signup").on("click", function(){ 
-        $(".signup-box").css("display", "none"); 
+        $("#signup-box").css("display", "none"); 
     });
 
+    
 });
+
+function clickCloser() {
+    $(document).one("click", function(e) {
+        if( e.target.id != 'signup-box') {
+            $("#signup-box").hide();
+                }
+            });
+        }
+
+function clickCloserTwo() {
+    $(document).one("click", function(e) {
+        if( e.target.class != 'login-form') {
+            $(".login-form").hide();
+                }
+            });
+        }
