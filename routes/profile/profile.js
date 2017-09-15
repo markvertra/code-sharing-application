70ba3a343c14b1a899aea5ba79160b0d03b16cb3
Mark Allen <mark@traversify.com>
2017-09-15 13:22:07 +0200

    Add my profile feature

--- routes/profile/profile.js
+++ routes/profile/profile.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/user');
 
+router.get('/myprofile', (req, res, next) => {
+  res.redirect('/profile/' + req.user._id)
+});
+
 router.get('/:profileId', (req, res, next) => {
   let profileId = req.params.profileId;
   User.findById(profileId).populate('projectIDs', 'projectName').exec((err, user) => {
@@ -9,5 +13,7 @@ router.get('/:profileId', (req, res, next) => {
     res.render('profile/profile', {user});
   });
 });
+
+
 
 module.exports = router;
