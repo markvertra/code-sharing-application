ce4e16344592256a28ed3944ee46e7445718e84d
Mark Allen <mark@traversify.com>
2017-09-15 11:57:24 +0200

    Overlays done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,9 +4,13 @@ $(document).ready(function(){
     // Files to appear and disappear login and sign-up
     $(".main-content").click(function(){
         $('#login-form').hide();
+      });
+
+    $('#main-overlay').click(function(){
+        $('#main-overlay').hide();
         $('#signup-box').hide();
         $('#project-signup-box').hide();
-      })
+      });
     
     $(".login-btn").on("click", function(){
         $("#login-form").css("display", "block");
@@ -15,11 +19,18 @@ $(document).ready(function(){
     $(".signup-link").on("click", function(){
         $("#signup-box").css("display", "flex");
         $("#login-form").css("display", "none"); 
+        $("#main-overlay").css("display", "block");
     });
+
+    $("#main-signup").on("click", function(){
+        $("#main-overlay").css("display", "none");
+    });
+
 
     // AJAX request to check user log-in status on new project
     
     $("#new-project-submit").on("click", ()=> {
+
         isUserLoggedIn();
         $("#secret-projectName").val($("#projectName").val());
         const $projectHTML = $("#project-HTML");
@@ -32,7 +43,6 @@ $(document).ready(function(){
         $projectHTML.appendTo($(".hidden-input"));
         $projectCSS.appendTo($(".hidden-input"));
         $projectJS.appendTo($(".hidden-input"));
-
     });
 
 
@@ -57,5 +67,6 @@ function isUserLoggedIn() {
 function serveSignup (bool) {
     if (!bool) {
         $("#project-signup-box").css("display", "flex");
+        $("#main-overlay").css("display", "block");
      } 
 }
--- routes/api/api.js
+++ routes/api/api.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 router.get('/user', (req, res, next) => {
     if (req.user) {
-      res.json({ user: true})
+      res.json({ user: true});
     } else
-      res.json({ user: false})
+      res.json({ user: false});
     
 });
 
